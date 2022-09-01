05437ddf54d159c577c70188059c57273a3e282a
Yohann BIGLIA <contact@yohannbiglia.fr>
2022-09-01 13:57:53 +0200

    build(docker): :bricks: Add swagger endpoint correctly

--- backend/DofitAPI/Program.cs
+++ backend/DofitAPI/Program.cs
@@ -1,5 +1,6 @@
 using DofitAPI.Models;
 using DofitAPI.Services;
+using DofitAPI.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,35 +9,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => { options.DocumentFilter<PathPrefixInsertDocumentFilter>("api"); });
 
 builder.Services.Configure<DofitDatabaseSettings>(builder.Configuration.GetSection("DofitDatabase"));
 builder.Services.AddSingleton<ItemsService>();
 builder.Services.AddSingleton<SellsService>();
 
 //services cors
-builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
-{
-    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-}));
+builder.Services.AddCors(p =>
+    p.AddPolicy("corsapp", builder => { builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader(); }));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-if (app.Environment.IsProduction())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.RoutePrefix = "api";
-        c.SwaggerEndpoint("/swagger.json", "DofitAPI");
-    });
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.UseCors("corsapp");
